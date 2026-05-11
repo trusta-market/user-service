e9e8e0b1091131064ffebb30ae5835f0b27e1946
ansdl <ansdlsgur0123@gmail.com>
2026-05-11 11:30:21 +0900

    [refactor]  HttpStatus 의존성 도메인에서 제거

--- src/main/java/com/trusta_market/userservice/report/domain/exception/ReportException.java
+++ src/main/java/com/trusta_market/userservice/report/domain/exception/ReportException.java
@@ -1,10 +1,14 @@
 package com.trusta_market.userservice.report.domain.exception;
 
-import com.trustamarket.common.exception.CustomException;
-import com.trustamarket.common.exception.ErrorCodeSpec;
+import lombok.Getter;
 
-public class ReportException extends CustomException {
-    public ReportException(ErrorCodeSpec errorCode) {
-        super(errorCode);
+// 신고 도메인 전역에서 사용하는 예외 클래스 (프레임워크 독립적)
+@Getter
+public class ReportException extends RuntimeException {
+    private final ReportErrorCode errorCode;
+
+    public ReportException(ReportErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
 }
