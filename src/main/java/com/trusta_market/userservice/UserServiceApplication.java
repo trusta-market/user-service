c3e0f0b1453eb4a89ce95c1ab3b8eb33174fae8f
ansdl <ansdlsgur0123@gmail.com>
2026-05-12 11:40:21 +0900

    Primary 추가 EnableFeignClients 추가

--- src/main/java/com/trusta_market/userservice/UserServiceApplication.java
+++ src/main/java/com/trusta_market/userservice/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(basePackages = { "com.trusta_market.userservice",
         "com.trustamarket.common" })
 @EnableJpaRepositories(basePackages = "com.trusta_market.userservice")
+@EnableFeignClients(basePackages = "com.trusta_market.userservice")
 @EntityScan(basePackages = {
         "com.trusta_market.userservice",
         "com.trustamarket.common"
--- src/main/java/com/trusta_market/userservice/user/infrastructure/persistence/jpa/config/JpaAuditConfig.java
+++ src/main/java/com/trusta_market/userservice/user/infrastructure/persistence/jpa/config/JpaAuditConfig.java
@@ -2,6 +2,7 @@ package com.trusta_market.userservice.user.infrastructure.persistence.jpa.config
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @EnableJpaAuditing
 public class JpaAuditConfig {
 
+    @Primary
     @Bean
     public AuditorAware<UUID> auditorProvider() {
         return () -> {
